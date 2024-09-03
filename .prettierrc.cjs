3980ac73dd5177f823cd60e0dc527aaf8735b3d9
Sten Levasseur <sten.levasseur@lonestone.io>
2024-09-03 22:17:53 +0200

    update config tailwind

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -1,3 +1,8 @@
+/** @typedef  {import("@ianvs/prettier-plugin-sort-imports").PluginConfig} SortImportsConfig*/
+/** @typedef  {import("prettier").Config} PrettierConfig*/
+/** @typedef  {{ tailwindConfig: string }} TailwindConfig*/
+
+/** @type { PrettierConfig | SortImportsConfig | TailwindConfig } */
 module.exports = {
   trailingComma: 'all', // Utilise des virgules en fin de ligne dans tous les contextes. Remplacez par 'es5' si vous voulez des virgules uniquement là où c'était valide en ES5 (c'est-à-dire dans les objets et les tableaux, mais pas dans les paramètres ou les arguments des fonctions).
   tabWidth: 2, // Utilise un espacement de 2 pour les tabulations.
@@ -5,4 +10,21 @@ module.exports = {
   singleQuote: true, // Utilise des guillemets simples au lieu de doubles.
   endOfLine: 'auto', // Gère automatiquement les fins de ligne en fonction de l'environnement, évitant les problèmes de compatibilité entre différents systèmes d'exploitation.
   arrowParens: 'always', // Encadre toujours les paramètres des fonctions fléchées avec des parenthèses.
+  plugins: [
+    "@ianvs/prettier-plugin-sort-imports",
+    "prettier-plugin-tailwindcss",
+  ],
+  tailwindConfig: './tailwind.config.js', 
+  importOrder: [
+    "^(react/(.*)$)|^(react$)|^(react-native(.*)$)", 
+    "<THIRD_PARTY_MODULES>",
+    "",
+    "^@ab-solutions/(.*)$",
+    "",
+    "^~/utils/(.*)$",
+    "^~/components/(.*)$",
+    "^~/(.*)$",
+    "^[./]",
+  ],
+  importOrderParserPlugins: ["typescript", "jsx", "decorators-legacy"],
 };
---
+++ tailwind.config.js
@@ -0,0 +1,85 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'night-blue': {
+          DEFAULT: 'hsl(230, 58%, 12%)',
+          light: 'hsl(230, 58%, 22%)',
+          dark: 'hsl(230, 58%, 2%)',
+        },
+        'dark-blue': 'hsl(230, 40%, 20%)',
+        'hard-blue': 'hsl(215, 95%, 32%)',
+        'electric-blue': 'hsl(215, 99%, 45%)',
+        'light-blue': 'hsl(230, 100%, 89%)',
+        'dark-red': 'hsl(0, 60%, 42%)',
+        'red': 'hsl(0, 80%, 63%)',
+        'green': 'hsl(165, 99%, 41%)',
+        'white': 'hsl(0, 0%, 100%)',
+      },
+      fontFamily: {
+        'chivo': ['Chivo', 'sans-serif'],
+        'overpass': ['Overpass', 'sans-serif'],
+      },
+      fontSize: {
+        'h1': '72px',
+        'h2': '36px',
+        'h3': '24px',
+        'h4': '24px',
+        'h5': '16px',
+        'texte-courant': '12px',
+      },
+      fontWeight: {
+        'normal': 400,
+        'black': 900,
+      },
+    },
+  },
+  plugins: [ 
+    function({ addBase, theme }) {
+      addBase({
+        'h1': {
+          fontFamily: theme('fontFamily.overpass'),
+          fontSize: theme('fontSize.h1'),
+          fontWeight: theme('fontWeight.black'),
+          textAlign: 'center',
+        },
+        'h2': {
+          fontFamily: theme('fontFamily.overpass'),
+          fontSize: theme('fontSize.h2'),
+          fontWeight: theme('fontWeight.black'),
+          textAlign: 'center',
+        },
+        'h3': {
+          fontFamily: theme('fontFamily.overpass'),
+          fontSize: theme('fontSize.h3'),
+          fontWeight: theme('fontWeight.black'),
+          textAlign: 'right',
+        },
+        'h4': {
+          fontFamily: theme('fontFamily.chivo'),
+          fontSize: theme('fontSize.h4'),
+          fontWeight: theme('fontWeight.normal'),
+          textAlign: 'center',
+        },
+        'h5': {
+          fontFamily: theme('fontFamily.overpass'),
+          fontSize: theme('fontSize.h5'),
+          fontWeight: theme('fontWeight.black'),
+          textAlign: 'center',
+        },
+        '.Texte-courant': {
+          fontFamily: theme('fontFamily.chivo'),
+          fontSize: theme('fontSize.texte-courant'),
+          fontWeight: theme('fontWeight.normal'),
+          textAlign: 'center',
+        },
+      })
+    },
+  ],
+}
