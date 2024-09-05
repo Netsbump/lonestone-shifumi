9c975cccce99825f965acc3d3b6b8cd962a42d73
Sten Levasseur <sten.levasseur@lonestone.io>
2024-09-05 20:28:34 +0200

    feat: update game button

--- src/ui/button/GameButton.tsx
+++ src/ui/button/GameButton.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import { Button } from 'react-aria-components';
 
+import { Choice } from '../../lib/types/global';
+
 type GameButtonProps = {
   children: React.ReactNode;
+  name: 'start-button' | Choice;
   icon?: React.ReactNode;
-  onPress?: () => void;
   isDisabled?: boolean;
+  onPress: (typeButton: 'start-button' | Choice) => void;
 };
 
 export const GameButton: React.FC<GameButtonProps> = ({
@@ -13,11 +16,12 @@ export const GameButton: React.FC<GameButtonProps> = ({
   icon,
   onPress,
   isDisabled,
+  name,
 }) => {
   return (
     <Button
-      className="h4 rounded-primary shadow-button-primary bg-button px-4 py-3"
-      onPress={onPress}
+      className="h4 rounded-primary bg-button px-4 py-3 shadow-button-primary"
+      onPress={() => onPress(name)}
       isDisabled={isDisabled}
     >
       {icon && <span>{icon}</span>}
